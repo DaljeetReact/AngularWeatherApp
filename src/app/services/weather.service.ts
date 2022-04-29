3c32e6243ca87396b511ee29ef4a5ea326a392dc
Singh <daljeet.test99@gmail.com>
2022-04-29 11:16:52 +0200

    add service and rapid api details

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {WeatherData} from '../models/weather.mdoel';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) {
+
+   }
+
+   getWeatherData(cityName: string):Observable<WeatherData> {
+     return this.http.get<WeatherData>(environment.WeatherApibaseUrl,{
+        params: {
+          q:cityName,
+          units: 'mertic',
+          mode: 'json' 
+        },
+        headers: {
+          'X-RapidAPI-Host': environment.RapidAPIHost,
+          'X-RapidAPI-Key':environment.RapidAPIKey
+        }
+      })
+   }
+}
