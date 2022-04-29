be4b237399b1d55ead125747777b8ddb4632b7d8
Singh <daljeet.test99@gmail.com>
2022-04-29 11:18:45 +0200

    bindling ui with components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {WeatherService} from './services/weather.service'
+import {WeatherData} from './models/weather.mdoel'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
+  constructor(private WeatherService:WeatherService){
+
+  }
+  weatherData?:WeatherData;
+  cityName:string='napoli';
+
+  ngOnInit(): void{
+    this.getWeatherData(this.cityName);
+    this.cityName='';
+  }
+  
+  private getWeatherData(CityName:string){
+    this.WeatherService.getWeatherData(CityName).subscribe({
+      next:(response) => {
+        this.weatherData  = response;
+      console.log(response)
+     }
+    });
+  }
+
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName='';
+  }
+
   title = 'AngularWeatherApp';
 }
