1167acfcfa45ba2b2dba782fdc7b932bbf79bebe
Singh <daljeet.test99@gmail.com>
2022-04-29 15:12:09 +0200

    set pod info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   WeatherApibaseUrl:"https://community-open-weather-map.p.rapidapi.com/weather",
   RapidAPIHost: 'community-open-weather-map.p.rapidapi.com',
   RapidAPIKey: 'f074a64f16msh0b559fcfb6b71f4p1dc602jsn91bb5100c3bf'
